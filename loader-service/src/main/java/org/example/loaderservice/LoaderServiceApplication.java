92dd2802e9a90cfdfce963d31a2e60147ae59b48
mustafa.shama <mustafa.shama@taboola.com>
2025-04-12 13:54:39 +0300

    init

---
+++ loader-service/src/main/java/org/example/loaderservice/LoaderServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.loaderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoaderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoaderServiceApplication.class, args);
+    }
+
+}
