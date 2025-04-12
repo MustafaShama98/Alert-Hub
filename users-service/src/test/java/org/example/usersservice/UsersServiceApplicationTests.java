37e399615b14ad229cdc361158adf1659ca5d80d
mustafa.shama <mustafa.shama@taboola.com>
2025-04-12 14:29:13 +0300

    fixed configs

--- users-service/src/test/java/org/example/usersservice/UsersServiceApplicationTests.java
+++ users-service/src/test/java/org/example/usersservice/UsersServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package org.example.userservice;
+package org.example.usersservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserServiceApplicationTests {
+class UsersServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
