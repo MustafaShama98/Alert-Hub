c4c88cb85efba716f98a7ebbe522bf7d1077b91e
mustafa.shama <mustafa.shama@taboola.com>
2025-04-12 13:58:22 +0300

    added evaulation-services

---
+++ evalulation-service/src/main/java/org/example/evalulationservice/EvalulationServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.evalulationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvalulationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvalulationServiceApplication.class, args);
+	}
+
+}
---
+++ evalulation-service/src/test/java/org/example/evalulationservice/EvalulationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.evalulationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvalulationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
