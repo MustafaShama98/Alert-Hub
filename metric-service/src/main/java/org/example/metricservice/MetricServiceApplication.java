18c122588c1e5da9c9ec5d746941e69a3d64eabb
mustafa.shama <mustafa.shama@taboola.com>
2025-05-02 00:00:58 +0300

    metric

---
+++ metric-service/src/main/java/org/example/metricservice/MetricServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.metricservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetricServiceApplication.class, args);
+    }
+
+}
