e52b35bc618de1832b92e6968e4c48586c561daf
mustafa.shama <mustafa.shama@taboola.com>
2025-05-05 11:11:08 +0300

    acttion

---
+++ action-scheduler-service/src/main/java/org/example/actionschedulerservice/ActionSchedulerServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.actionschedulerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionSchedulerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActionSchedulerServiceApplication.class, args);
+    }
+
+}
